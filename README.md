cd77313b1330b5f6088fae637993cb5d95ef0777
Dennis Micky Jensen <root@mewm.org>
2014-01-09 15:07:01 +0100

    Renamed repo, and cleaned up a bit

---
+++ README.md
@@ -0,0 +1,4 @@
+feed-music-scrapion
+===================
+
+Scrapes a room chat history for music links, downloads them, and converts them to mp3
---
+++ config-sample.js
@@ -0,0 +1,14 @@
+var config = {}
+
+//General
+config.outputFolder = './mp3/';
+
+//Hipchat feed
+config.hipchatApiKey = '';
+config.roomId = 00000;
+
+//Feed source
+config.feedSource = 'HipChat';
+
+module.exports = config;
+
