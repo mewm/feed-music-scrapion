426653a71cbc3456f7a1a6738878768d7c51daff
Dennis Micky Jensen <root@mewm.org>
2014-01-11 03:04:25 +0100

    Big improvements in stability (yes I know, sucky commit message)

--- config-sample.js
+++ config-sample.js
@@ -5,6 +5,7 @@ config.outputFolder = './mp3/';
 
 //Hipchat feed
 config.hipchatApiKey = '';
+config.roomNotificationKey = 'avQtc9cw3OBq8n7IB2DnkNSy3MBrHOZRShku8Cj1';
 config.roomId = 00000;
 
 //Feed source
